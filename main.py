a2248be6e91cd1d7c63559d39c3325b629c27230
keithkrazy1228 <keithtomita1@gmail.com>
2025-04-11 00:40:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ def hello():
 # GASからPOSTされたデータを受け取る場所
 @app.route('/api/parse', methods=['POST'])
 def parse_home4u():
-   data = request.json
+    data = request.json
 
-property_id = data.get("propertyId")
-contact_date = data.get("contactDate")
-detail_url = data.get("detailUrl")
-login_id = data.get("loginId")
-password = data.get("password")
+    property_id = data.get("propertyId")
+    contact_date = data.get("contactDate")
+    detail_url = data.get("detailUrl")
+    login_id = data.get("loginId")
+    password = data.get("password")
 
-print("▼ログインID：", login_id)
-print("▼パスワード：", password)
+    print("▼ログインID：", login_id)
+    print("▼パスワード：", password)
 
-print("査定ナンバー:", property_id)
-print("ご依頼日:", contact_date)
-print("詳細URL:", detail_url)
+    print("査定ナンバー:", property_id)
+    print("ご依頼日:", contact_date)
+    print("詳細URL:", detail_url)
 
     # ここから先にスクレイピング＆スプレッドシート登録処理を追加していく
     return jsonify({"status": "received", "propertyId": property_id})
